bc22a545679c348584b2e606dc656b1c46bb06c6
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-20 08:26:39 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,7 +3,6 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
-      t.integer :actor_id
       t.string :catchphrase
     end
   end
